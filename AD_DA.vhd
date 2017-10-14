a70597553c626fcda761fa617cf71f5ffda030ad
pokerfaceSad <pokerfacesad@126.com>
2017-10-14 14:56:39 +0800

    完成数据的接收

--- AD_DA.vhd
+++ AD_DA.vhd
@@ -14,10 +14,14 @@ entity AD_DA is
 		state_signal_1 : buffer std_logic_vector(3 downto 0); --state Out
 		
 		CLKNUM:buffer integer:=0;
-		DO_1 : buffer std_logic --Data Out
+		DO_1 : buffer std_logic; --Data Out
+		
+		DataA_output: out std_logic_vector(7 downto 0);
+		DataB_output: out std_logic_vector(7 downto 0);
+		Parallel_Data:out integer
 		);
 	signal DI_1 : std_logic:='0'; --Data In
-	signal CS_1 : std_logic:='0'; --CHIP Select
+	signal CS_1 : std_logic:='1'; --CHIP Select
 	--signal CH0_1: integer:=12; --analog data entrance
 	--signal CH1_1 : integer:=16; --analog data entrance
 end AD_DA;
@@ -34,6 +38,10 @@ architecture behavior of AD_DA is
 		);
 	END COMPONENT ADC0832;
 	
+	signal DataA : std_logic_vector(7 downto 0);
+	signal DataB : std_logic_vector(7 downto 0);
+	signal receive_order:std_logic;
+	shared VARIABLE COUNT:INTEGER:=0;
 begin 
 	u1:ADC0832 PORT MAP(clk=>clk_1, CH0=>CH0_1, CH1=>CH1_1, CS=>CS_1,DI=>DI_1,state_signal=>state_signal_1,DO=>DO_1);
 	
@@ -50,9 +58,10 @@ begin
 			case CLKNUM is 
 				when 0 => 
 					--CS_1 <= '1';
+					receive_order <= '0';
 					DI_1 <= '0';
 				when 1 => 
-					--CS_1 <= '1';
+					CS_1 <= '0';
 					DI_1 <= '0';
 				when 2 => 
 					--CS_1 <= '0';
@@ -69,12 +78,43 @@ begin
 					--Second DI
 					--CS_1 <= '0';
 					DI_1 <= '1';
-				when others =>
-					--CS_1 <= '0';
+				when 6 =>
+					--Transfrom
 					DI_1 <= '0';
+				when 7 =>
+					receive_order <= '1';
+					
+				when others =>
+					if(COUNT < 15) then
+						receive_order <= '1';
+					else
+						receive_order <= '0';
+					end if;
 			end case;
+			DataA_output <= DataA;
+			DataB_output <= DataB;
 			CS_output <= CS_1;
 			DI_output <= DI_1;
+			Parallel_Data <= CONV_INTEGER(DataA); 
+	end process;
+	
+	receive : process(receive_order,clk_1)
+	begin
+		if(receive_order = '1') then
+			if(clk_1'event and clk_1='0') then
+				if(COUNT<7) then
+					DataA <= DataA(6 downto 0) & DO_1;
+				end if;
+				if(COUNT=7) then
+					DataA <= DataA(6 downto 0) & DO_1;
+					DataB <= DO_1 & DataB(7 downto 1);
+				end if;
+				if(COUNT<15 and COUNT >7) then
+					DataB <= DO_1 & DataB(7 downto 1);
+				end if;
+				COUNT:=COUNT+1;
+			end if;
+		end if;
 	end process;
 end behavior;
 	
